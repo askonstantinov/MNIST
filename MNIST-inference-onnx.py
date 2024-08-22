0587641853013d77fccf5797eecc028180b5b599
askonstantinov <blackcron@gmail.com>
2024-08-22 17:10:17 +0300

    Initial commit

---
+++ MNIST-inference-onnx.py
@@ -0,0 +1,82 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+import torchvision.datasets
+from bokeh.plotting import figure
+from bokeh.io import show
+from bokeh.models import LinearAxis, Range1d
+import numpy as np
+import datetime
+import onnx
+import onnxruntime as ort
+
+
+batch_size = 100
+
+# Specific for MNIST integrated into PyTorch
+DATA_PATH = '/home/konstantinov/PycharmProjects/MNIST/mnist-data-path'
+MODEL_STORE_PATH = '/home/konstantinov/PycharmProjects/MNIST/model-store-path'
+
+# transforms to apply to the data
+trans = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
+
+# MNIST dataset
+train_dataset = torchvision.datasets.MNIST(root=DATA_PATH, train=True, transform=trans, download=True)
+test_dataset = torchvision.datasets.MNIST(root=DATA_PATH, train=False, transform=trans)
+
+# Data loader
+test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+
+with torch.no_grad():
+    correct = 0
+    total = 0
+    for images, labels in test_loader:
+
+        ### Объявим пустой tupple для наполнения в цикле
+        out_t = ()
+
+        ### Зададим параметры инициализации onnx модели и откроем сессию onnxruntime с подгрузкой обученной модели onnx
+        input = torch.randn(1, 1, 28, 28)
+        ort_sess = ort.InferenceSession('/home/konstantinov/PycharmProjects/MNIST/mnist-custom2.onnx')
+
+        ### Автоматизируем процесс присваивания ключей для словаря в цикле
+        input_name = ort_sess.get_inputs()[0].name
+
+        for i in range(0, len(images)):
+            print('Проверочное значение, должна быть цифра =', int(labels[i]))
+
+            # НИЖЕ - ПРИВЕДЕНИЕ КО ВХОДУ onnxruntime
+            k = images[i,:,:]
+            z = np.array(k)
+            j = np.expand_dims(z, axis=0)
+
+            # НИЖЕ - СТАРТ ЗАМЕРА ВРЕМЕНИ
+            start = datetime.datetime.now()
+
+            # НИЖЕ - ИНФЕРЕНС модели onnx силами onnxruntime
+            outputs = ort_sess.run(None, {input_name: j})
+
+            # НИЖЕ - КОНЕЦ ЗАМЕРА ВРЕМЕНИ
+            finish = datetime.datetime.now()
+
+            outputs_array = outputs[0][0]
+            print('Результат инференса: получена цифра =', np.argmax(outputs_array))
+            print('Затраты времени на отработку инференса (например, 0:00:00.000118 это 118 мкс): ' + str(finish - start))
+            print('--------------------')
+            print('Порядковый номер проверенной картинки в текущей пачке (всего 100 картинок) =', i+1)
+            print('--------------------')
+
+            outputs = torch.tensor(outputs)[0]
+            out_t = torch.cat((torch.tensor(out_t), outputs),0)
+
+        _, predicted = torch.max(out_t.data, 1)
+        total += labels.size(0)
+        print('########################################')
+        print('Номер проверенной пачки тестовых изображений (всего 100 пачек) =', int(total/100))
+        print('########################################')
+        correct += (predicted == labels).sum().item()
+
+    print('================================================================================')
+    print('================================================================================')
+    print('Test Accuracy of the model on the 10000 test images: {} %'.format((correct / total) * 100))
---
+++ MNIST-inference.py
@@ -0,0 +1,99 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+import torchvision.datasets
+from bokeh.plotting import figure
+from bokeh.io import show
+from bokeh.models import LinearAxis, Range1d
+import numpy as np
+import datetime
+
+
+batch_size = 100
+
+# Specific for MNIST integrated into PyTorch
+DATA_PATH = '/home/konstantinov/PycharmProjects/MNIST/mnist-data-path'
+MODEL_STORE_PATH = '/home/konstantinov/PycharmProjects/MNIST/model-store-path'
+
+# transforms to apply to the data
+trans = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
+
+# MNIST dataset
+train_dataset = torchvision.datasets.MNIST(root=DATA_PATH, train=True, transform=trans, download=True)
+test_dataset = torchvision.datasets.MNIST(root=DATA_PATH, train=False, transform=trans)
+
+# Data loader
+train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+
+# Convolutional neural network (two convolutional layers)
+class ConvNet(nn.Module):
+    def __init__(self):
+        super(ConvNet, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.drop_out = nn.Dropout()
+        self.fc1 = nn.Linear(16 * 4 * 7 * 7, 1000)
+        self.fc2 = nn.Linear(1000, 10)
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.reshape(out.size(0), -1)
+        out = self.drop_out(out)
+        out = self.fc1(out)
+        out = self.fc2(out)
+        return out
+
+
+model = ConvNet()
+
+# Test the model
+model.eval()
+
+### Подгрузка обученной модели .pt
+model.load_state_dict(torch.load('/home/konstantinov/PycharmProjects/MNIST/output/mnist-custom_model2.pt'))
+
+with torch.no_grad():
+    correct = 0
+    total = 0
+    for images, labels in test_loader:
+
+        for i in range(0, len(images)):
+            print('Проверочное значение, должна быть цифра =', int(labels[i]))
+
+            # НИЖЕ - СТАРТ ЗАМЕРА ВРЕМЕНИ
+            start = datetime.datetime.now()
+
+            # НИЖЕ - ИНФЕРЕНС нативной модели .pt
+            outputs = model(images)
+
+            # НИЖЕ - КОНЕЦ ЗАМЕРА ВРЕМЕНИ
+            finish = datetime.datetime.now()
+
+            ### Ниже отпечатываем результат инференса в каждой из 100 тест-пачек
+            print_outputs = outputs[i]
+            print_max_outputs = np.argmax(print_outputs)
+            print('Результат инференса: получена цифра =', print_max_outputs.item())
+            print('Затраты времени на отработку инференса (например, 0:00:00.000118 это 118 мкс): ' + str(finish - start))
+            print('--------------------')
+            print('Порядковый номер проверенной картинки в текущей пачке (всего 100 картинок) =', i+1)
+            print('--------------------')
+
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        print('########################################')
+        print('Номер проверенной пачки тестовых изображений (всего 100 пачек) =', int(total/100))
+        print('########################################')
+        correct += (predicted == labels).sum().item()
+
+    print('================================================================================')
+    print('================================================================================')
+    print('Test Accuracy of the model on the 10000 test images: {} %'.format((correct / total) * 100))
